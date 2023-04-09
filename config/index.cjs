213beae311a010bbebcc6594bcc828806c02ed4a
Ding Xinghui <68988724+hitApple@users.noreply.github.com>
2023-04-09 14:51:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o6YIw5uuDoQ1LEoutX8442OOjMBM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Mo1-dIlX2t3uG5k874Uy9nVVUVK2M2NuuJbrTNkYsbA',
+      useTemplateId: 'TgNcX19ZjL91lhL7GjLPdCLX-9GH5Kr3QRLgJW-gqcQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-03',
       festivals: [
