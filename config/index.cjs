723bcc38b93a7be6dd176ca60f094ef9bce875b9
Ding Xinghui <68988724+hitApple@users.noreply.github.com>
2023-04-24 22:46:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小浣熊',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o6YIw5uuDoQ1LEoutX8442OOjMBM',
+      id: 'o6YIw5j8vBykeDy3y1s_eaJHL9BI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'yzNE6ePuVc1SoS-vg_v38gKm2za5afzoE4RUHyy1LNY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
