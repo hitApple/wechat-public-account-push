55c16b2d3cd156846e5ff190d7cdcc644fe394e6
Ding Xinghui <68988724+hitApple@users.noreply.github.com>
2023-05-23 22:38:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,15 +23,21 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o6YIw5uuDoQ1LEoutX8442OOjMBM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'yzNE6ePuVc1SoS-vg_v38gKm2za5afzoE4RUHyy1LNY',
+      useTemplateId: 'Ayj4DPfAWzGS0su2kHvcuQpjW-kD_065uwttaaoiYH8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-03',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '小浣熊', year: '2000', date: '12-28',
+          type: '*生日', name: '小浣熊', year: '1999', date: '12-28',
         },
-
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '小浣熊', year: '2000', date: '02-03',
+        },
+        {
+          type: '节日', name: '交往纪念日', year: '2022', date: '07-31',
+        },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
@@ -39,12 +45,11 @@ const USER_CONFIG = {
         { keyword: 'love_day', date: '2022-07-31' },
       ],
     },
-
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'sqCvUHJ924nXKdaDxFKb46iNNNa_NW8mxnD6eWVaMVs',
+  CALLBACK_TEMPLATE_ID: 'Ayj4DPfAWzGS0su2kHvcuQpjW-kD_065uwttaaoiYH8',
 
   CALLBACK_USERS: [
     {
@@ -57,4 +62,3 @@ const USER_CONFIG = {
 }
 
 module.exports = USER_CONFIG
-
